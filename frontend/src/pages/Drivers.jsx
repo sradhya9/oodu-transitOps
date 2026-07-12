1d899eca6a61a9ed2d92651b9c427cd682e4668c
Vedha Mahadevan <159766539+Vedha17Mahadevan@users.noreply.github.com>
2026-07-12 11:45:42 +0530

    Add files via upload

---
+++ frontend/src/pages/Drivers.jsx
@@ -0,0 +1,9 @@
+const Drivers = () => {
+  return (
+    <div>
+      <h1>Driver</h1>
+    </div>
+  );
+};
+
+export default Drivers;
---
+++ frontend/src/pages/Trips.jsx
@@ -0,0 +1,9 @@
+const Trips = () => {
+  return (
+    <div>
+      <h1>Trips</h1>
+    </div>
+  );
+};
+
+export default Trips;
